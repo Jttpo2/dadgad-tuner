4173f5973715dbd6bca92c0caa1a247f99460dc8
John Petersson <johnpetersson@gmail.com>
2018-04-25 13:28:51 +0200

    Removed unused.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 import Constants from './Constants.js';
 import InstrumentString from './InstrumentString.js';
@@ -26,7 +24,6 @@ class App extends Component {
     return (
       <div style={styles.container}>
         <header style={styles.header}>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 style={styles.title}>DADGAD tuner</h1>
         </header>
         <Tuner strings={this._strings}></Tuner>
@@ -40,15 +37,12 @@ export default App;
 const styles = {
   container: {
       height: '100%',
-      // textAlign: 'center',
       display: 'flex',
       justifyContent: 'center',
       flexFlow: 'column'
   },
   header: {
     backgroundColor: Constants.primaryColor,
-    // height: '10%',
-    // padding: '20px',
     flex: 0.2,
     color: 'white',
 
