e675f373f5c5a895581a8d382e7c0f2ff6771ce2
John Petersson <johnpetersson@gmail.com>
2018-04-25 09:44:32 +0200

    Indentation.

--- src/NoteButton.js
+++ src/NoteButton.js
@@ -14,30 +14,30 @@ export class NoteButton extends Component {
   }
 
   onClick = (event) => {
-      this.props.onClick(event);
+    this.props.onClick(event);
   };
-
+  
   render() {
     return (
       <button
         style={styles.noteButton}
         onClick={this.onClick}>{this.state.displayName}</button>
-    );
+      );
+    }
   }
-}
 
-const diameter = '5rem';
-const styles = {
-  noteButton: {
-    background: Constants.noteButtonColor,
-    // flex: 0.07,
-    width: diameter,
-    height: diameter,
-    borderRadius: '50%',
+  const diameter = '5rem';
+  const styles = {
+    noteButton: {
+      background: Constants.noteButtonColor,
+      // flex: 0.07,
+      width: diameter,
+      height: diameter,
+      borderRadius: '50%',
 
-    fontSize: '2.5em',
-    color: 'white',
-    fontFamily: 'helvetica, sans-serif'
+      fontSize: '2.5em',
+      color: 'white',
+      fontFamily: 'helvetica, sans-serif'
 
+    }
   }
-}
