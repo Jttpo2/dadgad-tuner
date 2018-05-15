47b8ab28601039814b936631db560fddd199a5e5
John Petersson <johnpetersson@gmail.com>
2018-05-15 17:55:34 +0200

    Clean.

--- src/StringButton.js
+++ src/StringButton.js
@@ -58,7 +58,6 @@ class StringButton extends Component {
 
   const styles = {
     stringButton: {
-      background: Constants.stringButtonColor1,
       background: 'radial-gradient(' + Constants.stringButtonColor1 + ' ' + Constants.stringButtonGradientPercentage + ', ' + Constants.stringButtonColor2 + ')',
       // flex: 0.07,
       width: diameter + sizeUnit,
